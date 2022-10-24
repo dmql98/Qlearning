d1c7a47f57b8e1db211fc3138c35767ce19fdad0
dmql98 <duanmuqianlin@gmail.com>
2022-10-24 11:30:58 -0400

    1

--- main.py
+++ main.py
@@ -4,18 +4,18 @@ from Table import Table
 
 print('input here:')
 ## get the input from user
-filename = input("Enter file name: ")
-action = float(input("Enter reward for each action(non-positive): "))
-learningRate = float(input("Enter learning rate: "))
-gamma =  float(input("Enter Gamma: "))
-runTime = float(input("How many seconds to run for: "))
-P = float(input("Enter P(action succeeds) : "))
-# filename = 'map.csv'
-# action = -0.04
-# gamma = 0.9
-# learningRate = 0.1
-# runTime = 1.0
-# P = 0.9
+# filename = input("Enter file name: ")
+# action = float(input("Enter reward for each action(non-positive): "))
+# learningRate = float(input("Enter learning rate: "))
+# gamma =  float(input("Enter Gamma: "))
+# runTime = float(input("How many seconds to run for: "))
+# P = float(input("Enter P(action succeeds) : "))
+filename = 'map.csv'
+action = -0.04
+gamma = 0.9
+learningRate = 0.1
+runTime = 1.0
+P = 0.9
 print('===========================================')
 
 # init
@@ -39,8 +39,8 @@ agent.explore()
 # print the current agent location on map and if it is terminated
 # print('agent current location: ', agent.currentLocation, '//  terminated?', agent.terminated())
 
-# print('===========================', '\n Qtable')
-# agent.printMaxQTable()
+print('===========================', '\n Qtable')
+agent.printMaxQTable()
 
 # final result
 print('===========================================', '\n Policy')
